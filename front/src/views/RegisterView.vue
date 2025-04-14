45592c6e182b19b94ba644a1050b0022a52d6c22
Alexsik76 <o.sikorskyi.vn@gmail.com>
2025-04-14 14:54:03 +0300

    Displays error message

--- front/src/views/RegisterView.vue
+++ front/src/views/RegisterView.vue
@@ -11,12 +11,10 @@
           <router-link to="/" class="text-blue-500 hover:text-blue-700">На головну</router-link>
         </p>
         <p v-if="error" class="text-center text-red-600 text-sm">
-          Помилка: {{ error }}
+          Помилка: {{ error }}, {{ data }}.
         </p>
+        
       </div>
-      <p class="text-center text-red-600 text-sm">
-          Помилка: {{ error }} 
-        </p>
     </div>
   </template>
   
@@ -26,25 +24,32 @@ import UserRegisterForm from '../components/UserRegisterForm.vue';
 import { ref } from 'vue'
 import { useRouter } from 'vue-router';
 const router = useRouter();
-  
 const new_user = ref({
   name: '',
   email: '',
   password: ''
 });
+
 const API_URL = import.meta.env.VITE_APP_API_URL;
-const {execute, error} = useFetch(`${API_URL}/auth/register`, {
-    method: 'POST',
-    immediate: false,
-    afterFetch(ctx) {
-    if (ctx.data.title === 'Реєстрація успішна')
+const {execute, error, statusCode, data, onFetchError} = useFetch(`${API_URL}/auth/register`, {immediate:false, 
+  updateDataOnError: true
+}
+).post(new_user.value).json();
+  
+  async function register() {
+  
+    await execute(); // Виклик методів у правильному порядку
+
+    if (statusCode.value === 201) {
       router.push('/login');
-    else {
-      alert('Помилка реєстрації: ' + ctx.data.message + ctx.error);
+    } else if (statusCode.value === 400 && data.value && data.value.error) {
+      // errorMessage.value = data.value.error;
+      console.log("1", data.value, error.value, statusCode.value);
+    } else {
+      // errorMessage.value = 'Помилка реєстрації';
+      console.log("2", data.value, error.value, statusCode.value);
     }
-    return ctx
-    }}).post(new_user).json();
-  const register = () => {
-    execute();
-  };
+    
+}
+
   </script>
